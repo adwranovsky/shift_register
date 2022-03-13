8ec3208cfc22e672b522dcdfc8669dfca83238eb
Alexander Wranovsky <alex@wranovsky.org>
2022-03-12 20:36:24 -0600

    Demonstrate piso shift reg with a cover-generated testbench

--- rtl/shift_register_piso.v
+++ rtl/shift_register_piso.v
@@ -47,6 +47,27 @@ module shift_register_piso #(
     always @(posedge clk_i)
         f_past_valid <= 1;
 
+    // Create a testbench that sends the value 0xa5 out on bit_o
+    generate if (COVER==1 && WIDTH == 8) begin
+        reg [5:0] f_num_advance = 0;
+        reg [5:0] f_num_reset = 0;
+        reg [7:0] f_data = 0;
+        always @(posedge clk_i) begin
+            if (f_past_valid && $past(advance_i))
+                f_data <= {bit_o,f_data[7:1]};
+
+            f_num_reset <= f_num_reset + rst_i;
+
+            if (rst_i || set_i)
+                f_num_advance <= 0;
+            else
+                f_num_advance <= f_num_advance + advance_i;
+        end
+        always @(posedge clk_i)
+            cover(f_data==8'ha5 && f_num_advance>=8 && f_num_reset>=1);
+
+    end endgenerate
+
 `endif
 
 endmodule
